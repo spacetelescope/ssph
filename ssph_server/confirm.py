289c3fd3b340b29b9f9692352f1e0e1b24518b4f
Oi In Tam <oiin.tam@gmail.com>
2022-05-16 16:09:01 -0400

    log the old way

--- ssph_server/confirm.py
+++ ssph_server/confirm.py
@@ -93,7 +93,7 @@ def run():
         # the session authentication event id being validated
     signature = data["sig"].value
         # the hash computed by the client of the input for this request
-    _barf(data,f'ip-validation {remote_addr}')
+    _barf(data,'ip-validation %s' % str(remote_addr))
 
     ### write your own if statements here
     match = False
